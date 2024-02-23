2b39b66eff1dc03e45a5e3853a422d065091fe1e
Unshack <marckfriencecoto30@gmail.com>
2024-02-23 16:57:45 +0800

    queries save activity notification

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -31,6 +31,89 @@ export const getAuthUserDetails = async () => {
   return userData;
 };
 
+export const saveActivityLogsNotification = async ({
+  agencyId,
+  description,
+  subaccountId,
+}: {
+  agencyId?: string;
+  description: string;
+  subaccountId?: string;
+}) => {
+  const authUser = await currentUser();
+  let userData;
+  if (!authUser) {
+    const response = await db.user.findFirst({
+      where: {
+        Agency: {
+          SubAccount: { some: { id: subaccountId } },
+        },
+      },
+    });
+    if (response) {
+      userData = response;
+    }
+  } else {
+    userData = await db.user.findUnique({
+      where: { email: authUser?.emailAddresses[0].emailAddress },
+    });
+  }
+
+  if (!userData) {
+    console.log("Could not find a user");
+    return;
+  }
+
+  let foundAgencyId = agencyId;
+  if (!foundAgencyId) {
+    if (!subaccountId) {
+      throw new Error(
+        "You need to provide atleast an agency Id or subaccount Id"
+      );
+    }
+    const response = await db.subAccount.findUnique({
+      where: { id: subaccountId },
+    });
+    if (response) foundAgencyId = response.agencyId;
+  }
+  if (subaccountId) {
+    await db.notification.create({
+      data: {
+        notification: `${userData.name} | ${description}`,
+        User: {
+          connect: {
+            id: userData.id,
+          },
+        },
+        Agency: {
+          connect: {
+            id: foundAgencyId,
+          },
+        },
+        SubAccount: {
+          connect: { id: subaccountId },
+        },
+      },
+    });
+  } else {
+    await db.notification.create({
+      data: {
+        notification: `${userData.name} | ${description}`,
+        User: {
+          connect: {
+            id: userData.id,
+          },
+        },
+        Agency: {
+          connect: {
+            id: foundAgencyId,
+          },
+        },
+      },
+    });
+  }
+};
+
 export const createTeamUser = async (agencyId: string, user: User) => {
   if (user.role === "AGENCY_OWNER") return null;
   const response = await db.user.create({ data: { ...user } });
