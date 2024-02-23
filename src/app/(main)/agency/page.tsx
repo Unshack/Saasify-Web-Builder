9f7c316fc3f73f9820cb058277e5c8a5e43a0232
Unshack <marckfriencecoto30@gmail.com>
2024-02-23 17:18:32 +0800

    invitation authentication save user account

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -4,10 +4,8 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  const authUser = await currentUser();
-  if (!authUser) return redirect("/sign-in");
-
   const agencyId = await verifyAndAcceptInvitation();
+  console.log(agencyId);
   // get users details
   const user = await getAuthUserDetails();
   return <div>Agency Dashboard</div>;
