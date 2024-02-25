341a2883041dcfd7315817ea305e528c4f1c0b0a
Unshack <marckfriencecoto30@gmail.com>
2024-02-25 11:07:39 +0800

    sidebar initial functionality and bug error fix

--- src/components/sidebar/menu-options.tsx
+++ src/components/sidebar/menu-options.tsx
@@ -1,8 +1,31 @@
+"use client";
+
+import {
+  AgencySidebarOption,
+  SubAccount,
+  SubAccountSidebarOption,
+} from "@prisma/client";
 import React from "react";
 
-type Props = {};
+type Props = {
+  defaultOpen?: boolean;
+  subAccount: SubAccount[];
+  sidebarOpt: AgencySidebarOption[] | SubAccountSidebarOption[];
+  sidebarLogo: string;
+  details: any;
+  user: any;
+  id: string;
+};
 
-const MenuOptions = (props: Props) => {
+const MenuOptions = ({
+  details,
+  id,
+  sidebarLogo,
+  sidebarOpt,
+  subAccount,
+  user,
+  defaultOpen,
+}: Props) => {
   return <div>MenuOptions</div>;
 };
 
