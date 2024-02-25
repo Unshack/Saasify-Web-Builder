8cffc84134f7a78fd8b264c797c0ac7791bce838
Unshack <marckfriencecoto30@gmail.com>
2024-02-25 13:04:48 +0800

    manual context setup for modal functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import ModalProvider from "@/providers/modal-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = DM_Sans({ subsets: ["latin"] });
 
@@ -24,7 +26,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <ModalProvider>
+            {children}
+            <Toaster />
+          </ModalProvider>
         </ThemeProvider>
       </body>
     </html>
--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Agency, User } from "@prisma/client";
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 interface ModalProviderProps {
   children: React.ReactNode;
@@ -25,3 +25,51 @@ export const ModalContext = createContext<ModalContextType>({
   setOpen: (modal: React.ReactNode, fetchData?: () => Promise<any>) => {},
   setClose: () => {},
 });
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState<ModalData>({});
+  const [showingModal, setShowingModal] = useState<React.ReactNode>(null);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const setOpen = async (
+    modal: React.ReactNode,
+    fetchData?: () => Promise<any>
+  ) => {
+    if (modal) {
+      if (fetchData) {
+        setData({ ...data, ...(await fetchData()) } || {});
+      }
+      setShowingModal(modal);
+      setIsOpen(true);
+    }
+  };
+
+  const setClose = () => {
+    setIsOpen(false);
+    setData({});
+  };
+
+  if (!isMounted) return null;
+
+  return (
+    <ModalContext.Provider value={{ data, setOpen, setClose, isOpen }}>
+      {children}
+      {showingModal}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within the modal provider");
+  }
+  return context;
+};
+
+export default ModalProvider;
