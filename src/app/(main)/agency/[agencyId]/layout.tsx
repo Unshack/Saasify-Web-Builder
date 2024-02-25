d68521cd7ce4513064af9a229683161901b7f42e
Unshack <marckfriencecoto30@gmail.com>
2024-02-25 17:53:18 +0800

    notifications UI and functionality

--- src/app/(main)/agency/[agencyId]/layout.tsx
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -1,3 +1,5 @@
+import BlurPage from "@/components/global/blur-page";
+import Infobar from "@/components/global/infobar";
 import Sidebar from "@/components/sidebar";
 import Unauthorized from "@/components/unauthorized";
 import {
@@ -38,7 +40,12 @@ const layout = async ({ children, params }: Props) => {
   return (
     <div className="h-screen overflow-hidden">
       <Sidebar id={params.agencyId} type="agency" />
-      <div className="md:pl-[300px]">{children}</div>
+      <div className="md:pl-[300px]">
+        <Infobar notifications={allNoti} />
+        <div className="relative">
+          <BlurPage>{children}</BlurPage>
+        </div>
+      </div>
     </div>
   );
 };
